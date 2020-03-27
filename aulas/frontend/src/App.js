33741c0220a3f85f9c233fbe475580ba1cdbc7f3
pedrk <pedro.kondrat.ismart@gmail.com>
2020-03-27 13:53:17 -0300

    Frontend do app

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css'
+import Routes from './Routes'
+// JSX (JavaScript XML ) quando o html ta escrito no JavaScript
 
+/* Exemplo de useState 
+function App() {
+  let [counter, setCounter]  = useState(0);
+  
+  //useState retorn uma Array:
+  //Array [valor, funcaoAtualizaçãoDoValor]
+
+  function increment() {
+    setCounter(counter + 1)
+
+    console.log(counter)
+  } 
+  */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <Routes />
+  )
+};
 export default App;
