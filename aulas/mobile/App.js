5a03f1905bc37669e0b4b0c4fa93eec2133e87c0
pedrk <pedro.kondrat.ismart@gmail.com>
2020-03-27 21:22:35 -0300

    Mobile

---
+++ aulas/mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Routes from './src/routes';
+
+// Elementos comuns HTML: <div> <span> <header>
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
